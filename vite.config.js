301e935de6840d3a9b3cdc871255b306b06d1e61
sketchyz88 <dylanzimmerman809@gmail.com>
2026-04-08 23:49:48 -0700

    Add path alias for components to fix import resolution

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-    export default defineConfig({
-      plugins: [react()],
-    })
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+          alias: {
+                  './components': path.resolve(__dirname, 'src/components'),
+                  '../components': path.resolve(__dirname, 'src/components'),
+          }
+    }
+})

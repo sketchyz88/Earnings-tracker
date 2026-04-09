069221d876e4bda769a4f4171c0157fa62b7b4ae
sketchyz88 <dylanzimmerman809@gmail.com>
2026-04-09 00:26:54 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-          alias: {
-                  './components': path.resolve(__dirname, 'src/components'),
-                  '../components': path.resolve(__dirname, 'src/components'),
-          }
-    }
+  plugins: [react()],
 })
